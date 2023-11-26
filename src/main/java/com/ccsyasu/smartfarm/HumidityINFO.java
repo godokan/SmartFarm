586f21aeb6ca69be206782293d5bb21d607ba6ef
진홍기 <jjins.dev@gmail.com>
2023-11-27 01:55:46 +0900

    홈페이지 소스 수정

---
+++ src/main/java/com/ccsyasu/smartfarm/HumidityINFO.java
@@ -0,0 +1,25 @@
+package com.ccsyasu.smartfarm;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class HumidityINFO {
+    private static Integer HUMIDITY = 0;
+    private static String STATUS = "알 수 없음";
+
+    public static Integer getHUMIDITY() {
+        return HUMIDITY;
+    }
+
+    public static String getSTATUS() {
+        return STATUS;
+    }
+
+    public static void setHUMIDITY(Integer HUMIDITY) {
+        HumidityINFO.HUMIDITY = HUMIDITY;
+    }
+
+    public static void setSTATUS(String STATUS) {
+        HumidityINFO.STATUS = STATUS;
+    }
+}
