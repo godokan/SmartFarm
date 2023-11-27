5a903d5ae80319188d68f725381d7841de57416a
godokan <nonamekoo@naver.com>
2023-11-27 15:53:09 +0900

    near 최종 수정

--- src/main/java/com/ccsyasu/smartfarm/Controller/ClientController.java
+++ src/main/java/com/ccsyasu/smartfarm/Controller/ClientController.java
@@ -1,5 +1,7 @@
 package com.ccsyasu.smartfarm.Controller;
 
+import com.ccsyasu.smartfarm.Service.HumidityService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,13 +10,17 @@ import static com.ccsyasu.smartfarm.HumidityINFO.getSTATUS;
 
 @Controller
 public class ClientController {
+    @Autowired
+    HumidityService humidityService;
 
     @GetMapping("/index")
     public String test(Model model) {
 
-        model.addAttribute("name", "부들부들 애기 부들");
+        model.addAttribute("name", "바모 (스위트 바질)");
+        model.addAttribute("proper_name", "바모");
         model.addAttribute("humidity", getHUMIDITY());
         model.addAttribute("status", getSTATUS());
+        model.addAttribute("d-day", humidityService.D_DAY());
 
         return "index";
     }
--- src/main/java/com/ccsyasu/smartfarm/Service/HumidityService.java
+++ src/main/java/com/ccsyasu/smartfarm/Service/HumidityService.java
@@ -1,6 +1,10 @@
 package com.ccsyasu.smartfarm.Service;
 
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 import static com.ccsyasu.smartfarm.HumidityINFO.getHUMIDITY;
 import static com.ccsyasu.smartfarm.HumidityINFO.setSTATUS;
 
@@ -13,5 +17,11 @@ public class HumidityService {
         else if (current < 50) setSTATUS("마름");
         else if (current < 90) setSTATUS("촉촉함");
         else setSTATUS("충분함");
+    }
+
+    public long D_DAY() {
+        LocalDate dPlusDayDate = LocalDate.parse("2023-09-27", DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        LocalDate currentDate = LocalDate.now();
+        return currentDate.toEpochDay() - dPlusDayDate.toEpochDay();
     }
 }
